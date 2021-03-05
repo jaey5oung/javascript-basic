15f73aad308d9f7b49a735b5986bb562c7d83f50
장재영 <jaey5oung@naver.com>
2021-03-05 17:33:40 +0900

    기초반복6

---
+++ class.js
@@ -0,0 +1,95 @@
+'use strict';
+
+// 1. class declarations
+
+class Person {
+  //constructor
+  constructor(name, age) {
+    //fields
+    this.name = name;
+    this.age = age;
+  }
+  //methods
+  speak() {
+    console.log(`${this.name}: hello!`);
+  }
+}
+
+const yeonhee = new Person('yeonhee', 27);
+console.log(yeonhee.name);
+console.log(yeonhee.age);
+yeonhee.speak();
+
+//2. Getter and setters
+
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = firstName;
+    this.age = age;
+  }
+
+  get age() {
+    return this.age;
+  }
+
+  set age(value) {
+    // if (value < 0) {
+    //   throw Error('age can not be negative');
+    // }
+    this._age = value < 0 ? 0 : value;
+    this.age = value;
+  }
+}
+
+const user1 = new User('Stave', 'Job', -1);
+console.log(user1.age);
+
+//3.Fields
+
+class Experiment {
+  publicField = 2;
+  #privateField = 0;
+}
+
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+class Article {
+  static publisher = 'Dream Coding';
+  constructor(articleNumber) {
+    this.articleNumber = articleNumber;
+  }
+
+  static printPublisher() {
+    console.log(Article.publisher);
+  }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(article1.publicsher);
+
+class Shape {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw() {
+    console.log(`drawing ${this.color} color of`);
+  }
+  getArea() {
+    return width * this.height;
+  }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
