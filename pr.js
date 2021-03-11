90f11b250959f37bf88cd687d32a60dd5c98abaf
장재영 <jaey5oung@naver.com>
2021-03-11 19:50:24 +0900

    3/11 electorn build

---
+++ pr.js
@@ -0,0 +1,18 @@
+
+function add(num1,num2){
+    return num1+num2    
+}
+const result = add(2,3)
+console.log(result);
+
+function divide(num1,num2){
+    return num1/num2
+}
+const result2 = divide(2,3)
+console.log(result2);
+
+function suprise(op){
+    const result3 = op(2,3)
+    console.log(result3);
+    }
+suprise(divide)
